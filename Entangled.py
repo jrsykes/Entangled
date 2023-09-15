691730753514908c1d34332175afb46df4e415de
jrsykes <jrs596@york.ac.uk>
2023-09-15 22:26:35 +0100

    Now runs on CPU

--- Entangled.py
+++ Entangled.py
@@ -13,30 +13,33 @@ from torchvision import transforms
 from progress.bar import Bar
 import argparse
 
-# %% Define paths
-# images_dir = '/local/scratch/jrs596/dat/ElodeaProject/Elodea/Summer2022_Photos/BradGalenaCub_1/DCIM/100GOPRO'
-# out_path = "/local/scratch/jrs596/dat/ElodeaProject/Entangle_Detect_Out"
-
-
-# Binary_classifier_model_path = '/local/scratch/jrs596/dat/ElodeaProject/models/Entangled18.pth'
-# FaterRCNN_model_path = '/local/scratch/jrs596/dat/ElodeaProject/FasterRCNN_output/model_final.pth'
 
 #add command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--images_dir', type=str, help='path to images')
-parser.add_argument('--out_path', type=str, help='path to output')
+parser.add_argument('--out_path', default=None, type=str, help='path to output')
 parser.add_argument('--Binary_classifier_model_path', type=str, help='path to binary classifier model')
 parser.add_argument('--FaterRCNN_model_path', type=str, help='path to FasterRCNN model')
 
 args = parser.parse_args()
 
+
 #if cuda is available, use gpu otherwise use cpu
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+if args.out_path is None:
+    os.makedirs(os.path.join('.', 'Entangle_Detect_Out'), exist_ok=True)
+    
 # %% Load FasterRCNN model and custom weights
 
 cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+# Check for GPU availability and set the device accordingly
+if torch.cuda.is_available():
+    cfg.MODEL.DEVICE = 'cuda'
+else:
+    cfg.MODEL.DEVICE = 'cpu'
+
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9  
 cfg.MODEL.WEIGHTS = args.FaterRCNN_model_path  
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   
@@ -94,9 +97,16 @@ def FasterRCNN_Predict(image_file_name, image_directory, binary_classifier_model
 		shutil.copy(source, dest + '/' + image_file_name)
 
 #%%Load the "plant not plant" model
-Binary_classifier_model = torch.load(args.Binary_classifier_model_path)
+if torch.cuda.is_available():
+	Binary_classifier_model = torch.load(args.Binary_classifier_model_path)
+else:
+	Binary_classifier_model = torch.load(args.Binary_classifier_model_path, map_location=torch.device('cpu'))
 
 input_images = os.listdir(args.images_dir)
+if len(input_images) == 0:
+    print('No images found in the input directory')
+    exit(0)
+    
 with Bar('Disentangling...', max=len(input_images)) as bar:
 	for image in input_images:
 		FasterRCNN_Predict(image_file_name=image, image_directory=args.images_dir, binary_classifier_model=Binary_classifier_model)
